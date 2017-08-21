73edc6b404342f3cca5e9dae4eef39221ec2b443
Stephen Lowrie <stephen.lowrie@gmail.com>
2017-08-21 16:12:57 -0700

    Move nodes back inside stages. You win declarative pipelines.

--- os/tectonic-nightly.groovy
+++ os/tectonic-nightly.groovy
@@ -19,29 +19,29 @@ def default_builder_image = 'quay.io/coreos/tectonic-builder:v1.36'
 def tectonic_smoke_test_env_image = 'quay.io/coreos/tectonic-smoke-test-env:v3.0'
 
 
-node('amd64 && docker') {
-  pipeline {
-    agent none
-    options {
-      timeout(time:70, unit:'MINUTES')
-      timestamps()
-      buildDiscarder(logRotator(numToKeepStr:'100'))
-    }
-    parameters {
-      string(
-        name: 'builder_image',
-        defaultValue: default_builder_image,
-        description: 'tectonic-builder docker image to use for builds'
-      )
-    }
+pipeline {
+  agent none
+  options {
+    timeout(time:70, unit:'MINUTES')
+    timestamps()
+    buildDiscarder(logRotator(numToKeepStr:'100'))
+  }
+  parameters {
+    string(
+      name: 'builder_image',
+      defaultValue: default_builder_image,
+      description: 'tectonic-builder docker image to use for builds'
+    )
+  }
 
-    stages {
-      stage('Build & Test') {
-        environment {
-          GO_PROJECT = '/go/src/github.com/coreos/tectonic-installer'
-          MAKEFLAGS = '-j4'
-        }
-        steps {
+  stages {
+    stage('Build & Test') {
+      environment {
+        GO_PROJECT = '/go/src/github.com/coreos/tectonic-installer'
+        MAKEFLAGS = '-j4'
+      }
+      steps {
+        node('amd64 && docker') {
           withDockerContainer(params.builder_image) {
             sh '''#!/bin/bash -ex
             rm -rf tectonic-installer
@@ -63,55 +63,60 @@ node('amd64 && docker') {
             make test
             rm -fr frontend/tests_output
             '''
+            stash name: 'installer', includes: 'tectonic-installer/installer/bin/linux/installer'
+            stash name: 'smoke', includes: 'tectonic-installer/bin/smoke'
           }
         }
       }
+    }
 
-      stage("Tests") {
-        environment {
-          TECTONIC_INSTALLER_ROLE = 'tectonic-installer'
-        }
-        steps {
+    stage("Tests") {
+      environment {
+        TECTONIC_INSTALLER_ROLE = 'tectonic-installer'
+      }
+      steps {
+        node('amd64 && docker') {
           withCredentials(creds) {
+            unstash 'installer'
+            unstash 'smoke'
             withDockerContainer(tectonic_smoke_test_env_image) {
-              unstash 'installer'
-                sh '''#!/bin/bash -ex
-                  # Create aws key-pair
-                  sshdir=$(pwd)
-                  export TF_VAR_tectonic_aws_ssh_key="tectonic-nightly-$(mktemp -t 'XXXXXXXXXX' -u -p . | tr -d ./)"
-                  ssh-keygen -t rsa -b 4096 -f $sshdir/$TF_VAR_tectonic_aws_ssh_key -N "" -q
-                  kp=$(<$sshdir/$TF_VAR_tectonic_aws_ssh_key.pub)
-                  eval `ssh-agent -s`
-                  ssh-add $sshdir/$TF_VAR_tectonic_aws_ssh_key
-                  aws ec2 import-key-pair --key-name=$TF_VAR_tectonic_aws_ssh_key --public-key-material "$kp" --region us-west-2
+              sh '''#!/bin/bash -ex
+              # Create aws key-pair
+              sshdir=$(pwd)
+              export TF_VAR_tectonic_aws_ssh_key="tectonic-nightly-$(mktemp -t 'XXXXXXXXXX' -u -p . | tr -d ./)"
+              ssh-keygen -t rsa -b 4096 -f $sshdir/$TF_VAR_tectonic_aws_ssh_key -N "" -q
+              kp=$(<$sshdir/$TF_VAR_tectonic_aws_ssh_key.pub)
+              eval `ssh-agent -s`
+              ssh-add $sshdir/$TF_VAR_tectonic_aws_ssh_key
+              aws ec2 import-key-pair --key-name=$TF_VAR_tectonic_aws_ssh_key --public-key-material "$kp" --region us-west-2
 
-                  # Update the AMI
-                  COREOS_VERSION=$(curl -Ls "https://storage.googleapis.com/builds.developer.core-os.net/boards/amd64-usr/current-master/version.txt" | sed -n 's/^COREOS_VERSION=//p')
-                  AMI=$(curl -s "https://storage.googleapis.com/builds.developer.core-os.net/boards/amd64-usr/${COREOS_VERSION}/coreos_production_ami_all.json" | jq -r '.amis[] | select(.name == "us-west-2") | .hvm')
-                  sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/master-asg/master.tf
-                  sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/worker-asg/worker.tf
-                  sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/etcd/nodes.tf
+              # Update the AMI
+              COREOS_VERSION=$(curl -Ls "https://storage.googleapis.com/builds.developer.core-os.net/boards/amd64-usr/current-master/version.txt" | sed -n 's/^COREOS_VERSION=//p')
+              AMI=$(curl -s "https://storage.googleapis.com/builds.developer.core-os.net/boards/amd64-usr/${COREOS_VERSION}/coreos_production_ami_all.json" | jq -r '.amis[] | select(.name == "us-west-2") | .hvm')
+              sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/master-asg/master.tf
+              sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/worker-asg/worker.tf
+              sed -i "s/.{data.aws_ami.coreos_ami.image_id}/${AMI}/g" tectonic-installer/modules/aws/etcd/nodes.tf
 
-                  # Update the base domain in vars
-                  find tectonic-installer/tests/smoke/aws/vars/ -type f -exec sed -i "s|tectonic.dev.coreos.systems|clnightly.dev.coreos.systems|g" {} +;
-                  find tectonic-installer/tests/smoke/aws/vars/ -type f -exec sed -i "s|eu-west-1|us-west-2|g" {} +;
+              # Update the base domain in vars
+              find tectonic-installer/tests/smoke/aws/vars/ -type f -exec sed -i "s|tectonic.dev.coreos.systems|clnightly.dev.coreos.systems|g" {} +;
+              find tectonic-installer/tests/smoke/aws/vars/ -type f -exec sed -i "s|eu-west-1|us-west-2|g" {} +;
 
-                  sed -i "s|eu-west-1|us-west-2|g" tectonic-installer/examples/terraform.tfvars.aws
+              sed -i "s|eu-west-1|us-west-2|g" tectonic-installer/examples/terraform.tfvars.aws
 
-                  # Update the regions & base domain in smoke.sh
-                  sed -i "s|REGIONS=.*|REGIONS=(us-west-2)|g" tectonic-installer/tests/smoke/aws/smoke.sh
-                  sed -i "s|TF_VAR_base_domain=.*|TF_VAR_base_domain=clnightly.dev.coreos.systems|g" tectonic-installer/tests/smoke/aws/smoke.sh
+              # Update the regions & base domain in smoke.sh
+              sed -i "s|REGIONS=.*|REGIONS=(us-west-2)|g" tectonic-installer/tests/smoke/aws/smoke.sh
+              sed -i "s|TF_VAR_base_domain=.*|TF_VAR_base_domain=clnightly.dev.coreos.systems|g" tectonic-installer/tests/smoke/aws/smoke.sh
 
-                  cd tectonic-installer/tests/rspec
-                  bundler exec rubocop --cache false tests/rspec
-                  bundler exec rspec
+              cd tectonic-installer/tests/rspec
+              bundler exec rubocop --cache false tests/rspec
+              bundler exec rspec
 
-                  # Delete aws key-pair
-                  trap ssh-add -d $sshdir/$TF_VAR_tectonic_aws_ssh_key
-                  trap aws ec2 delete-key-pair --key-name $TF_VAR_tectonic_aws_ssh_key --region us-west-2
-                  trap rm $sshdir/$TF_VAR_tectonic_aws_ssh_key
-                  trap rm $sshdir/$TF_VAR_tectonic_aws_ssh_key.pub
-                '''
+              # Delete aws key-pair
+              trap ssh-add -d $sshdir/$TF_VAR_tectonic_aws_ssh_key
+              trap aws ec2 delete-key-pair --key-name $TF_VAR_tectonic_aws_ssh_key --region us-west-2
+              trap rm $sshdir/$TF_VAR_tectonic_aws_ssh_key
+              trap rm $sshdir/$TF_VAR_tectonic_aws_ssh_key.pub
+              '''
             }
           }
         }
